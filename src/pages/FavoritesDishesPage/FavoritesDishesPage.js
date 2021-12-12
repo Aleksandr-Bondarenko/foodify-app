98c7d77e45058ccf674da2e53ec6fdae37422051
Aleksandr-Bondarenko <slenderman.dnp@gmail.com>
2021-12-12 03:21:10 +0200

    adds notify text to favorites page

--- src/pages/FavoritesDishesPage/FavoritesDishesPage.js
+++ src/pages/FavoritesDishesPage/FavoritesDishesPage.js
@@ -6,20 +6,26 @@ function FavoritesDishesPage({ favoritesDishes, removeHandler }) {
   return (
     <div className={s.page}>
       <Container>
-        <ul className={s.list}>
-          {favoritesDishes.map(
-            ({ idMeal, strMeal, strMealThumb, strInstructions }) => (
-              <li className={s.item} key={idMeal}>
-                <DishCard
-                  title={strMeal}
-                  imgUrl={strMealThumb}
-                  desc={strInstructions}
-                  removeHandler={() => removeHandler(idMeal)}
-                />
-              </li>
-            )
-          )}
-        </ul>
+        {favoritesDishes && favoritesDishes.length !== 0 ? (
+          <ul className={s.list}>
+            {favoritesDishes.map(
+              ({ idMeal, strMeal, strMealThumb, strInstructions }) => (
+                <li className={s.item} key={idMeal}>
+                  <DishCard
+                    title={strMeal}
+                    imgUrl={strMealThumb}
+                    desc={strInstructions}
+                    removeHandler={() => removeHandler(idMeal)}
+                  />
+                </li>
+              )
+            )}
+          </ul>
+        ) : (
+          <p className={s.notify}>
+            You have not added anything to your favorites yet...
+          </p>
+        )}
       </Container>
     </div>
   );
